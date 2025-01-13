cea4ee75a412874ed77a33e3ec091a54820d4888
Yutao Hu <1123153758@qq.com>
2025-01-13 15:25:38 +0800

    训练：使用另外一张卡单独计算trepa_loss

--- scripts/train_unet.py
+++ scripts/train_unet.py
@@ -59,13 +59,13 @@ logger = get_logger(__name__)
 
 def main(config):
     # 初始化分布式训练
-    local_rank = init_dist()  # 获取本地进程的排名
-    global_rank = dist.get_rank()  # 获取全局进程的排名
-    num_processes = dist.get_world_size()  # 获取总进程数
-    is_main_process = global_rank == 0  # 判断是否为主进程
+    # local_rank = init_dist()  # 获取本地进程的排名
+    # global_rank = dist.get_rank()  # 获取全局进程的排名
+    # num_processes = dist.get_world_size()  # 获取总进程数
+    # is_main_process = global_rank == 0  # 判断是否为主进程
 
-    seed = config.run.seed + global_rank  # 设置随机种子
-    set_seed(seed)
+    # seed = config.run.seed + global_rank  # 设置随机种子
+    set_seed(config.run.seed)
 
     # 日志文件夹
     folder_name = "train" + datetime.datetime.now().strftime(f"-%Y_%m_%d-%H:%M:%S")
@@ -79,16 +79,19 @@ def main(config):
     )
 
     # 处理输出文件夹的创建
-    if is_main_process:
-        diffusers.utils.logging.set_verbosity_info()
-        os.makedirs(output_dir, exist_ok=True)  # 创建输出目录
-        os.makedirs(f"{output_dir}/checkpoints", exist_ok=True)  # 创建检查点目录
-        os.makedirs(f"{output_dir}/val_videos", exist_ok=True)  # 创建验证视频目录
-        os.makedirs(f"{output_dir}/loss_charts", exist_ok=True)  # 创建损失图表目录
-        shutil.copy(config.unet_config_path, output_dir)  # 复制配置文件
-        shutil.copy(config.data.syncnet_config_path, output_dir)  # 复制同步网络配置文件
+    # if is_main_process:
+    diffusers.utils.logging.set_verbosity_info()
+    os.makedirs(output_dir, exist_ok=True)  # 创建输出目录
+    os.makedirs(f"{output_dir}/checkpoints", exist_ok=True)  # 创建检查点目录
+    os.makedirs(f"{output_dir}/val_videos", exist_ok=True)  # 创建验证视频目录
+    os.makedirs(f"{output_dir}/loss_charts", exist_ok=True)  # 创建损失图表目录
+    shutil.copy(config.unet_config_path, output_dir)  # 复制配置文件
+    shutil.copy(config.data.syncnet_config_path, output_dir)  # 复制同步网络配置文件
+    # endif
 
-    device = torch.device(local_rank)  # 设置设备
+    # device = torch.device(local_rank)  # 设置设备
+    device = torch.device("cuda:0")  # 设置设备
+    trepa_device = torch.device("cuda:1")  # 设置TREPA设备
 
     noise_scheduler = DDIMScheduler.from_pretrained("configs")  # 初始化噪声调度器
 
@@ -145,14 +148,15 @@ def main(config):
             trainable_params.append(param)
 
     # 如果需要缩放学习率
-    if config.optimizer.scale_lr:
-        config.optimizer.lr = config.optimizer.lr * num_processes  # 根据进程数缩放学习率
+    # if config.optimizer.scale_lr:
+        # config.optimizer.lr = config.optimizer.lr * num_processes  # 根据进程数缩放学习率
 
     optimizer = torch.optim.AdamW(trainable_params, lr=config.optimizer.lr)  # 初始化优化器
 
-    if is_main_process:
-        logger.info(f"trainable params number: {len(trainable_params)}")  # 记录可训练参数数量
-        logger.info(f"trainable params scale: {sum(p.numel() for p in trainable_params) / 1e6:.3f} M")  # 记录可训练参数规模
+    # if is_main_process:
+    logger.info(f"trainable params number: {len(trainable_params)}")  # 记录可训练参数数量
+    logger.info(f"trainable params scale: {sum(p.numel() for p in trainable_params) / 1e6:.3f} M")  # 记录可训练参数规模
+    # end if
 
     # 启用xformers
     if config.run.enable_xformers_memory_efficient_attention:
@@ -167,20 +171,20 @@ def main(config):
 
     # 获取train dataset
     train_dataset = UNetDataset(config.data.train_data_dir, config)  # 初始化数据集
-    distributed_sampler = DistributedSampler(
-        train_dataset,
-        num_replicas=num_processes,
-        rank=global_rank,
-        shuffle=True,
-        seed=config.run.seed,
-    )  # 初始化分布式采样器
+    # distributed_sampler = DistributedSampler(
+    #     train_dataset,
+    #     num_replicas=num_processes,
+    #     rank=global_rank,
+    #     shuffle=True,
+    #     seed=config.run.seed,
+    # )  # 初始化分布式采样器
 
     # 创建dataloader
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=config.data.batch_size,
-        shuffle=False,
-        sampler=distributed_sampler,
+        shuffle=True, # False,
+        # sampler=distributed_sampler,
         num_workers=config.data.num_workers,
         pin_memory=False,
         drop_last=True,
@@ -205,7 +209,7 @@ def main(config):
         lpips_loss_func = lpips.LPIPS(net="vgg").to(device)  # 初始化LPIPS损失函数
 
     if config.run.trepa_loss_weight != 0 and config.run.pixel_space_supervise:
-        trepa_loss_func = TREPALoss(device=device, ckpt_path="./checkpoints/auxiliary/vit_g_hybrid_pt_1200e_ssv2_ft.pth")  # 初始化TREPA损失函数
+        trepa_loss_func = TREPALoss(device=trepa_device, ckpt_path="./checkpoints/auxiliary/vit_g_hybrid_pt_1200e_ssv2_ft.pth")  # 初始化TREPA损失函数
 
     # validation pipeline
     pipeline = LipsyncPipeline(
@@ -217,22 +221,26 @@ def main(config):
     pipeline.set_progress_bar_config(disable=True)  # 禁用进度条
 
     # DDP包装器
-    unet = DDP(unet, device_ids=[local_rank], output_device=local_rank)  # 包装UNet以支持分布式数据并行
+    # unet = DDP(unet, device_ids=[local_rank], output_device=local_rank)  # 包装UNet以支持分布式数据并行
 
     # 重新计算每个epoch的更新步骤
     num_update_steps_per_epoch = math.ceil(len(train_dataloader))  # 计算每个epoch的更新步骤
     num_train_epochs = math.ceil(config.run.max_train_steps / num_update_steps_per_epoch)  # 计算训练epoch数量
 
     # 开始训练
-    total_batch_size = config.data.batch_size * num_processes  # 计算总批量大小
+    # total_batch_size = config.data.batch_size * num_processes  # 计算总批量大小
+    total_batch_size = config.data.batch_size
 
-    if is_main_process:
-        logger.info("***** Running training *****")  # 记录训练开始信息
-        logger.info(f"  Num examples = {len(train_dataset)}")  # 记录训练样本数量
-        logger.info(f"  Num Epochs = {num_train_epochs}")  # 记录训练epoch数量
-        logger.info(f"  Instantaneous batch size per device = {config.data.batch_size}")  # 记录每个设备的批量大小
-        logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")  # 记录总批量大小
-        logger.info(f"  Total optimization steps = {config.run.max_train_steps}")  # 记录总优化步骤
+    # if is_main_process:
+    logger.info("***** Running training *****")  # 记录训练开始信息
+    logger.info(f"  Num examples = {len(train_dataset)}")  # 记录训练样本数量
+    logger.info(f"  Num Epochs = {num_train_epochs}")  # 记录训练epoch数量
+    logger.info(f"  Instantaneous batch size per device = {config.data.batch_size}")  # 记录每个设备的批量大小
+    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")  # 记录总批量大小
+    logger.info(f"  Total optimization steps = {config.run.max_train_steps}")  # 记录总优化步骤
+    # end if
+
+    resume_global_step = 0
     global_step = resume_global_step  # 初始化全局步骤
     first_epoch = resume_global_step // num_update_steps_per_epoch  # 计算第一个epoch
 
@@ -241,7 +249,7 @@ def main(config):
         range(0, config.run.max_train_steps),
         initial=resume_global_step,
         desc="Steps",
-        disable=not is_main_process,
+        # disable=not is_main_process,
     )
 
     train_step_list = []  # 训练步骤列表
@@ -255,7 +263,7 @@ def main(config):
     scaler = torch.cuda.amp.GradScaler() if config.run.mixed_precision_training else None  # 初始化梯度缩放器
 
     for epoch in range(first_epoch, num_train_epochs):
-        train_dataloader.sampler.set_epoch(epoch)  # 设置当前epoch
+        # train_dataloader.sampler.set_epoch(epoch)  # 设置当前epoch
         unet.train()  # 设置UNet为训练模式
 
         for step, batch in enumerate(train_dataloader):
@@ -368,21 +376,22 @@ def main(config):
                 pred_images = vae.decode(
                     rearrange(pred_latents, "b c f h w -> (b f) c h w") / vae.config.scaling_factor
                     + vae.config.shift_factor
-                ).sample  # 解码预测图像
+                ).sample  # 解码预测图像 [16, 3, 256, 256]
 
             # LPIPS Loss
             if config.run.perceptual_loss_weight != 0 and config.run.pixel_space_supervise:
-                pred_images_perceptual = pred_images[:, :, pred_images.shape[2] // 2 :, :]  # 获取感知图像
-                gt_images_perceptual = gt_images[:, :, gt_images.shape[2] // 2 :, :]  # 获取真实图像
+                pred_images_perceptual = pred_images[:, :, pred_images.shape[2] // 2 :, :]  # 获取感知图像 [16, 3, 128, 256]
+                gt_images_perceptual = gt_images[:, :, gt_images.shape[2] // 2 :, :]  # 获取真实图像 [16, 3, 128, 256]
                 lpips_loss = lpips_loss_func(pred_images_perceptual.float(), gt_images_perceptual.float()).mean()  # 计算LPIPS损失
             else:
                 lpips_loss = 0  # 如果不需要感知损失，设置为0
 
             # TREPA Loss
             if config.run.trepa_loss_weight != 0 and config.run.pixel_space_supervise:
-                trepa_pred_images = rearrange(pred_images, "(b f) c h w -> b c f h w", f=config.data.num_frames)  # 重排预测图像
-                trepa_gt_images = rearrange(gt_images, "(b f) c h w -> b c f h w", f=config.data.num_frames)  # 重排真实图像
-                trepa_loss = trepa_loss_func(trepa_pred_images, trepa_gt_images)  # 计算TREPA损失
+                trepa_pred_images = rearrange(pred_images, "(b f) c h w -> b c f h w", f=config.data.num_frames)  # 重排预测图像 [1, 3, 16, 256, 256]
+                trepa_gt_images = rearrange(gt_images, "(b f) c h w -> b c f h w", f=config.data.num_frames)  # 重排真实图像 [1, 3, 16, 256, 256]
+                trepa_loss = trepa_loss_func(trepa_pred_images.to(trepa_device), trepa_gt_images.to(trepa_device))  # 计算TREPA损失
+                trepa_loss = trepa_loss.to(device)
             else:
                 trepa_loss = 0  # 如果不需要TREPA损失，设置为0
 
@@ -399,7 +408,9 @@ def main(config):
                 ones_tensor = torch.ones((config.data.batch_size, 1)).float().to(device=device)  # 创建全1张量
                 vision_embeds, audio_embeds = syncnet(syncnet_input, mel)  # 获取视觉和音频嵌入
                 sync_loss = cosine_loss(vision_embeds.float(), audio_embeds.float(), ones_tensor).mean()  # 计算同步损失
-                sync_loss_list.append(gather_loss(sync_loss, device))  # 收集同步损失
+                # sync_loss_list.append(gather_loss(sync_loss, device))  # 收集同步损失
+                sync_loss_list.append(sync_loss.item())
+
             else:
                 sync_loss = 0  # 如果不需要同步损失，设置为0
 
@@ -412,7 +423,8 @@ def main(config):
 
             train_step_list.append(global_step)  # 添加当前步骤到训练步骤列表
             if config.run.recon_loss_weight != 0:
-                recon_loss_list.append(gather_loss(recon_loss, device))  # 收集重建损失
+                # recon_loss_list.append(gather_loss(recon_loss, device))  # 收集重建损失
+                recon_loss_list.append(recon_loss.item())
 
             optimizer.zero_grad()  # 清零梯度
 
@@ -442,7 +454,8 @@ def main(config):
             ### <<<< 训练 <<<< ###
 
             # 保存检查点并进行验证
-            if is_main_process and (global_step % config.ckpt.save_ckpt_steps == 0):
+            # if is_main_process and (global_step % config.ckpt.save_ckpt_steps == 0):
+            if global_step % config.ckpt.save_ckpt_steps == 0:
                 if config.run.recon_loss_weight != 0:
                     plot_loss_chart(
                         os.path.join(output_dir, f"loss_charts/recon_loss_chart-{global_step}.png"),
@@ -455,12 +468,12 @@ def main(config):
                             ("Sync loss", train_step_list, sync_loss_list),  # 绘制同步损失图表
                         )
                 model_save_path = os.path.join(output_dir, f"checkpoints/checkpoint-{global_step}.pt")  # 检查点保存路径
-                # state_dict = {
-                #     "global_step": global_step,
-                #     "state_dict": unet.module.state_dict(),  # 只保存未冻结的参数
-                # }
+                if isinstance(unet, torch.nn.DataParallel) or  isinstance(unet, torch.nn.parallel.DistributedDataParallel):
+                    state_dict = unet.module.state_dict()  # 获取模型状态字典
+                else:
+                    state_dict = unet.state_dict()  # 获取模型状态字典
                 try:
-                    torch.save(unet.module.state_dict(), model_save_path)  # 保存模型状态字典
+                    torch.save(state_dict, model_save_path)  # 保存模型状态字典
                     logger.info(f"Saved checkpoint to {model_save_path}")  # 记录保存信息
                 except Exception as e:
                     logger.error(f"Error saving model: {e}")  # 记录保存错误
@@ -517,23 +530,28 @@ if __name__ == "__main__":
 
     # 配置文件路径
     parser.add_argument("--unet_config_path", type=str, default="configs/unet/second_stage.yaml")
-    parser.add_argument("--finetune", type=bool, default=True)
-    parser.add_argument("--finetune_checkpoint_path", type=str, default="./finetune_outputs/train01/train-2025_01_10-09:46:31/checkpoints/checkpoint-1000.pt")
+    parser.add_argument("--finetune", type=bool, default=False)
+    parser.add_argument("--finetune_checkpoint_path", type=str, default="./finetune_outputs/train01/stage1/checkpoints/checkpoint-1000.pt")
     parser.add_argument("--train_data_dir", type=str, default="./0finetune_datas/high_visual_quality/")
     parser.add_argument("--val_video_path", type=str, default="./0finetune_datas/c0118-1080p-10s.mp4")
     parser.add_argument("--val_audio_path", type=str, default="./0finetune_datas/kanghui_train_30s.mp3")
+    parser.add_argument("--output_dir", type=str, default="./finetune_outputs/train01/stage2-notrepa/")
 
     args = parser.parse_args()  # 解析命令行参数
     config = OmegaConf.load(args.unet_config_path)  # 加载配置
     config.unet_config_path = args.unet_config_path  # 设置配置文件路径
 
+    if torch.cuda.device_count() < 2 and args.unet_config_path.endwith("second_stage.yaml"):
+        raise ValueError("At least 2 GPUs are required for training")
+
     if args.finetune:
-        config.data.train_output_dir = "finetune_outputs/train01/"  # 设置训练输出目录
+        config.data.train_output_dir = args.output_dir  # 设置训练输出目录
         config.data.train_data_dir = args.train_data_dir  # 设置训练数据目录
         config.data.val_video_path = args.val_video_path  # 设置验证视频路径
         config.data.val_audio_path = args.val_audio_path  # 设置验证音频路径
 
         config.run.finetune = True
+        config.optimizer.lr = 1e-5
         config.run.max_train_steps = 1000  # 设置最大训练步数
         config.ckpt.save_ckpt_steps = 500  # 设置保存检查点步数
         config.ckpt.resume_ckpt_path = args.finetune_checkpoint_path
